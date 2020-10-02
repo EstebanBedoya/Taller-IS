381af2864e70ce18c41900bf5409be9de76c73b8
EstebanBedoya <estebegi@gmail.com>
2020-10-01 20:26:11 -0500

    3 pruebas implementadas

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 function salario(horas, valorHora) {
-
+    const soloInt = /^\d*$/ //expresion regular para solo permitir valores enteros positivos
     let informe = { 'bruto': 0, 'extra': 0, 'neto': 0 }
 
-    if (typeof (horas) == 'number' && typeof (valorHora) == 'number') {
+    if (!soloInt.test(horas) || !soloInt.test(valorHora)) return 'valores invalidos'
 
-        if (horas <= 60 && horas >= 0 && valorHora >= 0) {
-            informe['bruto'] = horas * valorHora
-            if (horas > 48) {
-                informe['extra'] = informe['bruto'] + (valorHora * 0.2) * (horas - 48)
-            }
-            informe['neto'] = informe['bruto'] + informe['neto']
-            return informe
-
-        } else {
-            return 'valores invalidos'
+    if (horas <= 60) {
+        informe['bruto'] = horas * valorHora
+        if (horas > 48) {
+            informe['extra'] = informe['bruto'] + (valorHora * 0.2) * (horas - 48)
         }
-    } else {
-        return 'tipo de variables invalidas'
+        informe['neto'] = informe['bruto'] + informe['neto']
+        return informe
     }
 
 }
 
-
+console.log(salario(40, 30000));
 module.exports = salario
--- index.test.js
+++ index.test.js
@@ -1,6 +1,13 @@
-const { TestScheduler } = require('jest')
 const salario = require('./index')
 
-test('test para probar los test', () =>{
-    expect(salario(5,5)).toBe([{"horas":5, "valorHora":5}])
+test('no permitir valores flotantes en las horas', () =>{
+    expect(salario(5.3,30000)).toMatch('valores invalidos')
+})
+
+test('si son 48 horas o menos de trabajo, no se pagan horas extras', () =>{
+    expect(salario(40,30000)).toEqual({ bruto: 1200000, extra: 0, neto: 1200000 })
+})
+
+test('no valores negativos en horas', () =>{
+    expect(salario(-20,30000)).toMatch('valores invalidos')
 })
